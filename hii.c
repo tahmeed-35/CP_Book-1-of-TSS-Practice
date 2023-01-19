057ff415201443ff78537745fb2b86bd68d43ce8
tahmeed-35 <108896976+tahmeed-35@users.noreply.github.com>
2023-01-20 00:44:38 +0600

    test

---
+++ hii.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>   
+ int main()   
+ {   
+     int n;   
+     n = -0;   
+     if(n >= 0) {   
+         printf("The number is positive\n");   
+     }   
+     else {   
+         printf("The number is negative\n");   
+     }   
+     return 0;   
+ }  
